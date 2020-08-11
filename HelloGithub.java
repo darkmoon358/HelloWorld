8b864d60ecd415db117aa7e097aeab7c048ffd1f
darkmoon358 <923210776@qq.com>
2020-08-11 16:11:04 +0800

    modify hello github

---
+++ HelloGithub.java
@@ -0,0 +1,10 @@
+ public class HelloWorld {
+  public void sayhello() {
+   System.out.println("Hello World!!!"); 
+  }
+  
+  public static void main (String[] args) {
+   new HelloWorld().sayhello(); 
+  }
+ }
+ 
